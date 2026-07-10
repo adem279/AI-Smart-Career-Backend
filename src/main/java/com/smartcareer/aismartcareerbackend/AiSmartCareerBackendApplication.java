381e486c7db246c245aeccca696f8aae72c7cdd6
Adem Sahbeni <SAHBANI.Adem@esprit.tn>
2026-07-10 06:44:53 +0100

    Initial Spring Boot project

---
+++ src/main/java/com/smartcareer/aismartcareerbackend/AiSmartCareerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.smartcareer.aismartcareerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiSmartCareerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiSmartCareerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smartcareer/aismartcareerbackend/AiSmartCareerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartcareer.aismartcareerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiSmartCareerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
